2cc0caede2e41e1362eed2082bd7ae83192182a0
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/CORE_JS_SHIM_URL_.js
+++
@@ -1,6 +0,0 @@
-import { core_js_version_ } from './core_js_version_';
-export function CORE_JS_SHIM_URL_() {
-    return `https://cdnjs.cloudflare.com/ajax/libs/core-js/${core_js_version_()}/shim.min.js`;
-}
-export { CORE_JS_SHIM_URL_ as _CORE_JS_SHIM_URL, };
-//# sourceMappingURL=src/CORE_JS_SHIM_URL_.js.map
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './CORE_JS_SHIM_URL_';
-export * from './core_js_version_';
-export * from './CORE_JS_SHIM_URL';
-//# sourceMappingURL=src/index.js.map
